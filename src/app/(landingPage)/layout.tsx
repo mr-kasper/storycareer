50fce77e3d580f4abbe3f586515485efe02fedd3
mr-kasper <rachidwork5050@gmail.com>
2025-02-03 11:18:30 +0100

    footer

--- src/app/(landingPage)/layout.tsx
+++ src/app/(landingPage)/layout.tsx
@@ -1,11 +1,12 @@
 import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Navbar />
       <main className="h-full">{children}</main>
-      footer
+      <Footer />
     </div>
   );
 };
